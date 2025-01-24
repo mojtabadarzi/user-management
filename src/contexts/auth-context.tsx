7ed57d13174667b93d926047aa9ada6f55c7cee8
mojtaba <darzimojtaba@gmail.com>
2025-01-24 11:27:02 +0330

    feat(context): add context

---
+++ src/contexts/auth-context.tsx
@@ -0,0 +1,12 @@
+import { createContext, useContext } from 'react';
+import { AuthContextProps } from '../types/global';
+
+export const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuth must be used within AuthProvider');
+    }
+    return context;
+};
